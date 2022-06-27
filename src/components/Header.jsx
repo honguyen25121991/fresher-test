e0676a5c9c6a6c6fa0c9ff07e22118c2cbd5f62e
honguyen25121991@gmail.com <honguyen25121991@gmail.com>
2022-06-27 11:06:27 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = (props) => {
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             />
-            <span> Manager User App</span>
+            <span> Manager User App1</span>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
